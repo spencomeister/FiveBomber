5127ff2881f61880d13c8a5cebca00f4e1ead1eb
spencomeister <mk@seragl.io>
2024-02-07 13:00:41 +0900

    Add project files.

---
+++ FiveBomber/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace FiveBomber
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.progressBar3 = new System.Windows.Forms.ProgressBar();
+            this.progressBar4 = new System.Windows.Forms.ProgressBar();
+            this.progressBar5 = new System.Windows.Forms.ProgressBar();
+            this.btnStartStop = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 18);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(719, 30);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(12, 65);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(719, 30);
+            this.progressBar2.TabIndex = 0;
+            // 
+            // progressBar3
+            // 
+            this.progressBar3.Location = new System.Drawing.Point(12, 113);
+            this.progressBar3.Name = "progressBar3";
+            this.progressBar3.Size = new System.Drawing.Size(719, 30);
+            this.progressBar3.TabIndex = 0;
+            // 
+            // progressBar4
+            // 
+            this.progressBar4.Location = new System.Drawing.Point(12, 161);
+            this.progressBar4.Name = "progressBar4";
+            this.progressBar4.Size = new System.Drawing.Size(719, 30);
+            this.progressBar4.TabIndex = 0;
+            // 
+            // progressBar5
+            // 
+            this.progressBar5.Location = new System.Drawing.Point(12, 208);
+            this.progressBar5.Name = "progressBar5";
+            this.progressBar5.Size = new System.Drawing.Size(719, 30);
+            this.progressBar5.TabIndex = 0;
+            // 
+            // btnStartStop
+            // 
+            this.btnStartStop.Location = new System.Drawing.Point(767, 18);
+            this.btnStartStop.Name = "btnStartStop";
+            this.btnStartStop.Size = new System.Drawing.Size(122, 43);
+            this.btnStartStop.TabIndex = 1;
+            this.btnStartStop.Text = "Start / Stop";
+            this.btnStartStop.UseVisualStyleBackColor = true;
+            this.btnStartStop.Click += new System.EventHandler(this.btnStartStop_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(767, 78);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(122, 43);
+            this.btnNext.TabIndex = 1;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(767, 137);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(122, 43);
+            this.btnReset.TabIndex = 1;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(914, 259);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnStartStop);
+            this.Controls.Add(this.progressBar5);
+            this.Controls.Add(this.progressBar4);
+            this.Controls.Add(this.progressBar3);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "Form1";
+            this.Text = "FiveBomberEmulator";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.ProgressBar progressBar3;
+        private System.Windows.Forms.ProgressBar progressBar4;
+        private System.Windows.Forms.ProgressBar progressBar5;
+        private System.Windows.Forms.Button btnStartStop;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
+
---
+++ FiveBomber/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Threading;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace FiveBomber
+{
+    public partial class Form1 : Form
+    {
+        // This field stores an array of ProgressBar controls.
+        private readonly ProgressBar[] _progressBars;
+        // This field stores a reference to the System.Threading.Timer object.
+        private readonly System.Threading.Timer _timerField;
+        // This field stores a reference to the currently running System.Threading.Timer object.
+        private System.Threading.Timer _timer;
+        // This field stores an array of boolean values indicating whether each ProgressBar is processing.
+        private readonly bool[] _isProcessing;
+
+        // This method is called when the timer fires.
+        private void OnTimer(object state)
+        {
+            // Iterate through each ProgressBar.
+            for (int i = 0; i < _progressBars.Length; i++)
+            {
+                // If the ProgressBar is processing and its value is less than 100,
+                if (_isProcessing[i] && _progressBars[i].Value < 100)
+                {
+                    // Invoke the ProgressBar's Value property setter on the main thread.
+                    _progressBars[i].Invoke((MethodInvoker)delegate
+                    {
+                        // Increment the ProgressBar's value.
+                        _progressBars[i].Value++;
+                    });
+                }
+
+                // If the ProgressBar's value has reached 100,
+                if (_progressBars[i].Value == 100)
+                {
+                    // Set the processing flag for the ProgressBar to false.
+                    _isProcessing[i] = false;
+                    // Check if all ProgressBars have finished processing.
+                    bool isAllStopped = true;
+                    foreach (bool isProcessing in _isProcessing)
+                    {
+                        // If any ProgressBar is still processing,
+                        if (isProcessing)
+                        {
+                            // Set the isAllStopped flag to false and break out of the loop.
+                            isAllStopped = false;
+                            break;
+                        }
+                    }
+
+                    // If all ProgressBars have finished processing,
+                    if (isAllStopped)
+                    {
+                        // Dispose the timer.
+                        _timer.Dispose();
+                    }
+                }
+            }
+        }
+
+        public Form1()
+        {
+            // Initialize the form.
+            InitializeComponent();
+
+            // Initialize the _progressBars array with the ProgressBar controls on the form.
+            _progressBars = new[] {
+                progressBar1, progressBar2, progressBar3, progressBar4, progressBar5
+            };
+
+            // Initialize the _isProcessing array with a boolean value of true for each ProgressBar.
+            _isProcessing = new bool[_progressBars.Length];
+            for (int i = 0; i < _isProcessing.Length; i++)
+            {
+                _isProcessing[i] = true;
+            }
+        }
+
+        // This method is called when the Start/Stop button is clicked.
+        private void btnStartStop_Click(object sender, EventArgs e)
+        {
+            // If the timer is not null,
+            if (_timer != null)
+            {
+                // Stop the timer.
+                _timer.Change(Timeout.Infinite, Timeout.Infinite);
+                // Dispose the timer.
+                _timer.Dispose();
+                // Set the _timer field to null.
+                _timer = null;
+            }
+            else
+            {
+                // Create a new System.Threading.Timer object that fires every 1000 milliseconds.
+                _timer = new System.Threading.Timer(OnTimer, null, 1000, 1000);
+            }
+
+            // Set the processing flag for each ProgressBar to true.
+            for (int i = 0; i < _isProcessing.Length; i++)
+            {
+                _isProcessing[i] = true;
+            }
+        }
+
+        // This method is called when the Next button is clicked.
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            // Iterate through each ProgressBar.
+            for (int i = 0; i < _progressBars.Length; i++)
+            {
+                // If the ProgressBar is processing,
+                if (_isProcessing[i])
+                {
+                    // Set the processing flag for the ProgressBar to false.
+                    _isProcessing[i] = false;
+
+                    // Return from the method.
+                    return;
+                }
+            }
+        }
+
+        // This method is called when the Reset button is clicked.
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            // Iterate through each ProgressBar.
+            foreach (var progressBar in _progressBars)
+            {
+                // Set the ProgressBar's value to 0.
+                progressBar.Value = 0;
+            }
+            // Set the processing flag for each ProgressBar to true.
+            for (int i = 0; i < _isProcessing.Length; i++)
+            {
+                _isProcessing[i] = true;
+            }
+        }
+    }
+}
